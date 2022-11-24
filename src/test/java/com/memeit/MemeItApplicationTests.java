5132277213d07c7e3e58e179b2e648458afd640d
Bartlomiej <szybiakbartlomiej@gmail.com>
2022-11-24 11:54:38 +0100

    Create Project for memeIT

---
+++ src/test/java/com/memeit/MemeItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memeit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemeItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
