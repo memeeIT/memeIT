ae5b355fe73f9af9c68f67a70109c66d1a37f42d
Bartlomiej <szybiakbartlomiej@gmail.com>
2022-11-24 12:44:29 +0100

    full User RestAPI CRUD

--- src/main/java/com/memeit/common/PageMappingInfo.java
+++ src/main/java/com/memeit/common/PageMappingInfo.java
@@ -2,6 +2,15 @@ package com.memeit.common;
 
 public class PageMappingInfo {
 
-
+    // api
     public static final String API_PATH = "/api";
+
+    // users
+    public static final String USER_API_PATH = "/users";
+    public static final String UUID_PATH = "/{uuid}";
+    public static final String USER_UUID_PATH = PageMappingInfo.USER_API_PATH + PageMappingInfo.UUID_PATH;
+
+    // authentication
+    public static final String LOGIN_PAGE = "/login";
+    public static final String REGISTER_PAGE = "/register";
 }
